1777f0f3cead669f4dc9b0b64958be9d0b0c4723
Michael Laszlo <git@michaellaszlo.com>
2016-11-15 21:49:16 -0500

    Import mikelib and delete redundancies

--- js/league.js
+++ js/league.js
@@ -32,58 +32,25 @@ var League = (function () {
     });
   }
 
-  function make(tag, options) {
-    var element = document.createElement(tag);
-    [ 'id', 'className', 'innerHTML' ].forEach(function (property) {
-      if (property in options) {
-        element[property] = options[property];
-      }
-    });
-    if ('parent' in options) {
-      options.parent.appendChild(element);
-    }
-    return element;
-  }
-
-  function addClass(element, name) {
-    if (element.className) {
-      element.className += ' ' + name;
-    } else {
-      element.className = name;
-    }
-  }
-
-  function removeClass(element, name) {
-    var names = element.className.split(/\s+/),
-        newNames = [],
-        i;
-    for (i = 0; i < names.length; ++i) {
-      if (names[i] !== name) {
-        newNames.push(names[i]);
-      }
-    }
-    element.className = newNames.join(' ');
-  }
-
   function clickPlayer() {
     var player = this.player,
         spotlight = containers.spotlight;
     if (activePlayer !== null) {
-      removeClass(activePlayer.element, 'active');
+      M.classRemove(activePlayer.element, 'active');
     }
     activePlayer = player;
-    addClass(activePlayer.element, 'active');
+    M.classAdd(activePlayer.element, 'active');
     spotlight.innerHTML = '';
-    make('div', { className: 'player', parent: spotlight,
+    M.make('div', { className: 'player', parent: spotlight,
         innerHTML: player.name });
-    make('div', { className: 'record', parent: spotlight,
+    M.make('div', { className: 'record', parent: spotlight,
         innerHTML: player.wins + '-' + player.losses + '<br>' });
     player.history.forEach(function (match) {
-      var container = make('div', { className: 'match', parent: spotlight });
-      make('div', { parent: container,
+      var container = M.make('div', { className: 'match', parent: spotlight });
+      M.make('div', { parent: container,
           className: 'result ' + (match.result == 'W' ? 'win' : 'loss'),
           innerHTML: match.result });
-      make('div', { className: 'opponent', parent: container,
+      M.make('div', { className: 'opponent', parent: container,
           innerHTML: match.opponent.name });
       container.player = match.opponent;
       container.onclick = clickPlayer;
@@ -91,11 +58,11 @@ var League = (function () {
   }
 
   function load() {
-    containers.wrapper = make('div', { id: 'wrapper',
+    containers.wrapper = M.make('div', { id: 'wrapper',
         parent: document.body });
-    containers.players = make('div', { id: 'players',
+    containers.players = M.make('div', { id: 'players',
         parent: containers.wrapper });
-    containers.spotlight = make('div', { id: 'spotlight',
+    containers.spotlight = M.make('div', { id: 'spotlight',
         parent: containers.wrapper });
     players.sort(function (a, b) {
       if (a.name < b.name) {
@@ -106,7 +73,7 @@ var League = (function () {
       return 1;
     });
     players.forEach(function (player) {
-      var element = make('div', { className: 'player',
+      var element = M.make('div', { className: 'player',
           innerHTML: player.name,
           parent: containers.players });
       player.element = element;
